43ab4858e40ec051ffca5ffb60f89eb6c3b8855e
Rahul Salvi <rsalvi006@me.com>
2019-07-25 11:10:55 -0700

    add motor outputs to draw_plots.py

--- scripts/draw_plots.py
+++ scripts/draw_plots.py
@@ -21,13 +21,18 @@ def main(args):
     z_actual = []
     d_actual = []
     lvx = []
+    m0 = []
+    m1 = []
+    m2 = []
+    m3 = []
+    m4 = []
+    m5 = []
     killed = []
     if len(args) < 2:
         print("need input file")
         return
     with open(args[1], 'r') as f:
-        reader = csv.DictReader(f, delimiter=' ')
-        reader.fieldnames.pop(0)
+        reader = csv.DictReader(f)
         for row in reader:
             t = datetime.strptime(" ".join([row['date'], row['time']]), "%Y-%m-%d %H:%M:%S.%f")
             timestamps.append(t)
@@ -39,6 +44,12 @@ def main(args):
             y_actual.append(float(row['s_apy']))
             z_actual.append(float(row['s_apz']))
             d_actual.append(float(row['s_lpz']))
+            m0.append(float(row['motor0']))
+            m1.append(float(row['motor1']))
+            m2.append(float(row['motor2']))
+            m3.append(float(row['motor3']))
+            m4.append(float(row['motor4']))
+            m5.append(float(row['motor5']))
             lvx.append(float(row['m_lvx']))
             killed.append(bool(float(row['killed'])))
 
@@ -61,10 +72,12 @@ def main(args):
                y_axis_label="orientation (degrees)",
                y_range=(-180,180),
                sizing_mode="stretch_both")
-    p.extra_y_ranges = {"lvx": Range1d(start=-150,end=150), "depth": Range1d(start=4, end=-1)}
+    p.extra_y_ranges = {"lvx": Range1d(start=-150,end=150),
+                        "depth": Range1d(start=4, end=-1),
+                        "motors": Range1d(start=-400, end=400)}
     p.add_layout(LinearAxis(y_range_name="lvx", axis_label="velocity"), 'left')
     p.add_layout(LinearAxis(y_range_name="depth", axis_label="depth (meters)"), 'left')
-
+    p.add_layout(LinearAxis(y_range_name="motors", axis_label="motors"), 'left')
 
     colors = itertools.cycle(Category10[10])
     p.line(timestamps, x_actual, color=next(colors), legend="x_actual")
@@ -76,11 +89,16 @@ def main(args):
     p.line(timestamps, z_setpoint, color=next(colors), legend="z_setpoint")
     p.line(timestamps, d_setpoint, color=next(colors), legend="d_setpoint", y_range_name="depth")
     p.line(timestamps, lvx, color=next(colors), legend="lvx", y_range_name="lvx")
+    p.line(timestamps, m0, color=next(colors), legend="m0", y_range_name="motors")
+    p.line(timestamps, m1, color=next(colors), legend="m1", y_range_name="motors")
+    p.line(timestamps, m2, color=next(colors), legend="m2", y_range_name="motors")
+    p.line(timestamps, m3, color=next(colors), legend="m3", y_range_name="motors")
+    p.line(timestamps, m4, color=next(colors), legend="m4", y_range_name="motors")
+    p.line(timestamps, m4, color=next(colors), legend="m4", y_range_name="motors")
 
     for i in range(len(rising_edges)):
         box = BoxAnnotation(left=rising_edges[i], right=falling_edges[i], fill_color='red')
         p.add_layout(box)
-
 
     p.legend.location = "top_right"
     p.legend.click_policy = "hide"
